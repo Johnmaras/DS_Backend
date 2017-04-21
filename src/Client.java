ef9e2daf027494e355a8280697427fb2cac393ad
Johnmaras <giannis19961@windowslive.com>
2017-04-21 15:24:11 +0300

    client done. halts while waiting for the answer

--- src/Client.java
+++ src/Client.java
@@ -23,36 +23,37 @@ public class Client {
             }catch(IOException e){
                 System.err.println("Client_main: There was an IO error");
             }
-        }
-        while(true){
-            //TODO check if connection is still valid
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Give the string you want to search: ");
-            String query = scanner.nextLine();
-            Message request = new Message(9, query);
-            try{
-                ObjectOutputStream out = new ObjectOutputStream(MasterCon.getOutputStream());
-                out.writeObject(request);
-                out.flush();
-                if(query.equals("quit")) break;
-                ObjectInputStream in = null;
-                while(in == null){
-                    try{
-                        in = new ObjectInputStream(MasterCon.getInputStream());
-                        request = (Message)in.readObject();
-                        ArrayList<String> data = request.getData();
-                        data.forEach(System.out::println);
-                    }catch(NullPointerException e){
-                        System.err.println("Null Pointer!");
-                    }catch(SocketTimeoutException e){
-                        System.err.println("Socket Time Out!");
-                    }catch(ClassNotFoundException e){
-                        System.err.println("Class Not Found");
+            while(true){
+                //TODO check if connection is still valid
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Give the string you want to search: ");
+                String query = scanner.nextLine();
+                Message request = new Message(9, query);
+                try{
+                    ObjectOutputStream out = new ObjectOutputStream(MasterCon.getOutputStream());
+                    out.writeObject(request);
+                    out.flush();
+                    if(query.equals("quit")) break;
+                    ObjectInputStream in = null;
+                    while(in == null){
+                        try{
+                            in = new ObjectInputStream(MasterCon.getInputStream());
+                            request = (Message)in.readObject();
+                            ArrayList<String> data = request.getData();
+                            data.forEach(System.out::println);
+                        }catch(NullPointerException e){
+                            System.err.println("Null Pointer!");
+                        }catch(SocketTimeoutException e){
+                            System.err.println("Socket Time Out!");
+                        }catch(ClassNotFoundException e){
+                            System.err.println("Class Not Found");
+                        }
                     }
+                }catch(IOException e){
+                    System.err.println("Worker_connectToMaster: There was an IO error on openServer");
                 }
-            }catch(IOException e){
-                System.err.println("Worker_connectToMaster: There was an IO error on openServer");
             }
         }
+
     }
 }
