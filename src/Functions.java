d93be298dbe1233f3dfcd447128d5f570df2514c
Johnmaras <giannis19961@windowslive.com>
2017-04-21 11:29:44 +0300

    SocketAddress worker_id version

--- src/Functions.java
+++ src/Functions.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
@@ -11,7 +12,7 @@ public class Functions{
     private Hashtable<String, String> cache; //term(key) and depth in the file(value)
 
     private final File workers_file; //only the master node has workers file
-    private Hashtable<String, Socket> workers;
+    private Hashtable<String, SocketAddress> workers;
 
     public Functions(Object node){
         this.node = node;
@@ -21,7 +22,7 @@ public class Functions{
         this.workers = (this.node.getClass().getName().equals("Master") ? loadWorkers() : null);
     }
 
-    public Hashtable<String, Socket> getWorkers(){ return workers;}
+    public Hashtable<String, SocketAddress> getWorkers(){ return workers;}
 
     private Hashtable<String, String> loadCache(){
         createCache();
@@ -84,12 +85,12 @@ public class Functions{
         return cache.get(query);
     }
 
-    private Hashtable<String, Socket> loadWorkers(){
+    private Hashtable<String, SocketAddress> loadWorkers(){
         createWorkers();
         try{
             synchronized(workers_file) {
                 FileInputStream f = new FileInputStream(workers_file);
-                return (Hashtable<String, Socket>) new ObjectInputStream(f).readObject();
+                return (Hashtable<String, SocketAddress>) new ObjectInputStream(f).readObject();
             }
         }catch(IOException e){
             System.err.println("Master_loadWorkers: IOException occurred");
@@ -103,7 +104,7 @@ public class Functions{
 
     private void createWorkers(){
         if(!checkFile(workers_file)){
-            Hashtable<String, Socket> temp = new Hashtable<>();
+            Hashtable<String, SocketAddress> temp = new Hashtable<>();
             try{
                 synchronized(workers_file){
                     FileOutputStream f = new FileOutputStream(workers_file);
@@ -118,10 +119,10 @@ public class Functions{
         }
     }
 
-    public void updateWorkers(String worker_id, Socket worker_con){
+    public void updateWorkers(String worker_id, SocketAddress worker_con){
         workers.put(worker_id, worker_con);
         try{
-            Hashtable<String, Socket> temp = loadWorkers();
+            Hashtable<String, SocketAddress> temp = loadWorkers();
             temp.putAll(workers);
             synchronized(workers_file){
                 FileOutputStream c = new FileOutputStream(workers_file);
@@ -141,17 +142,15 @@ public class Functions{
     }
 
     public String hash(){
-        //TODO not finished
-        //TODO check inheritance problems on class_name
         String super_class = node.getClass().getSuperclass().getSimpleName();
         String class_name = (super_class.equals("Object") ? node.getClass().getSimpleName() : super_class);
         if(class_name.equals("Master")){
-            return ((Master)node).hash();
+            return "master_" + ((Master)node).hash();
         }else if(class_name.equals("Worker")){
-            return ((Worker)node).hash();
+            return "worker_" + ((Worker)node).hash();
         }else if(class_name.equals("Reducer")){
             //TODO reducer
-            //return ((Reducer)node).hash();
+            //return "reducer_" + ((Reducer)node).hash();
         }
         return null;
     }
