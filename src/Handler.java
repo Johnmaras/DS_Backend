de7d1ad39d7ec6bc8ece8b1414f14015aab4ae05
Johnmaras <giannis19961@windowslive.com>
2017-04-23 19:26:41 +0300

    sum commit. everything works fine

--- src/Handler.java
+++ src/Handler.java
@@ -17,7 +17,7 @@ public class Handler{
                     ObjectInputStream in = new ObjectInputStream(fi);
                     Hashtable<String, String> cache = (Hashtable<String, String>) in.readObject();
                     for(String key : cache.keySet()){
-                        System.out.println("key = " + key + " value = " + key.hashCode());
+                        System.out.println("key = " + key + " value = " + cache.get(key));
                     }
                     fi.close();
                     in.close();
