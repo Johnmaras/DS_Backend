ffc3c28a841ed1f67cfd7a5bb32e2c4c7f843318
Johnmaras <giannis19961@windowslive.com>
2017-04-23 13:46:53 +0300

    faster connection to worker

--- src/MW_Search.java
+++ src/MW_Search.java
@@ -15,42 +15,42 @@ public class MW_Search implements Runnable{
     @Override
     public void run() {
         Message request = new Message(1, query);
-        Socket WorkerCon = null;
-        while(WorkerCon == null){
-            try{
-                //System.out.println(worker_id);
-                WorkerCon = new Socket(InetAddress.getByName(worker_id), 4002);
-                ObjectOutputStream out = new ObjectOutputStream(WorkerCon.getOutputStream());
-                out.writeObject(request);
-                out.flush();
+        try{
+            //System.out.println(worker_id);
+            //TODO take long to throw exception. make it faster. DONE
+            InetSocketAddress worker = new InetSocketAddress(InetAddress.getByName(worker_id), 4002);
+            Socket WorkerCon = new Socket();
+            WorkerCon.connect(worker, 1000);
+            ObjectOutputStream out = new ObjectOutputStream(WorkerCon.getOutputStream());
+            out.writeObject(request);
+            out.flush();
 
-                //waits for answer from the worker
-                ObjectInputStream in = null;
-                while(in == null){
-                    try{
-                        in = new ObjectInputStream(WorkerCon.getInputStream());
-                        request = (Message)in.readObject();
-                        if(request.getRequestType() == 5){
-                            System.out.print(System.nanoTime() + " worker done.");
-                        }
-                    }catch(NullPointerException e){
-                        System.err.println("MW_Search_run: Null Pointer!");
-                    }catch(SocketTimeoutException e){
-                        System.err.println("MW_Search_run: Socket Time Out!");
-                    }catch(ClassNotFoundException e){
-                        System.err.println("MW_Search_run: Class Not Found");
+            //waits for answer from the worker
+            ObjectInputStream in = null;
+            while(in == null){
+                try{
+                    in = new ObjectInputStream(WorkerCon.getInputStream());
+                    request = (Message)in.readObject();
+                    if(request.getRequestType() == 5){
+                        System.out.print(System.nanoTime() + " worker done.");
                     }
+                }catch(NullPointerException e){
+                    System.err.println("MW_Search_run: Null Pointer!");
+                }catch(SocketTimeoutException e){
+                    System.err.println("MW_Search_run: Socket Time Out!");
+                }catch(ClassNotFoundException e){
+                    System.err.println("MW_Search_run: Class Not Found");
                 }
-            }catch(UnknownHostException e){
-                System.err.println("MW_Search_run: You are trying to connect to an unknown host!");
-                e.printStackTrace();
-            }catch(IOException e){
-                System.err.println("MW_Search_run: There was an IO error on openServer");
-                e.printStackTrace();
-            }catch(NullPointerException e){
-                System.err.println("MW_Search_run: NullPointer");
-                e.printStackTrace();
             }
+        }catch(UnknownHostException e){
+            System.err.println("MW_Search_run: You are trying to connect to an unknown host!");
+            e.printStackTrace();
+        }catch(IOException e){
+            System.err.println("MW_Search_run: There was an IO error. Host " + worker_id + " seems to be down!");
+        }catch(NullPointerException e){
+            System.err.println("MW_Search_run: NullPointer");
+            e.printStackTrace();
         }
+
     }
 }
