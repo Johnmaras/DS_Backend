883991db4015b98e40348119a582d8d6c6de816a
John <mitromarasg96@gmail.com>
2017-06-13 22:58:29 +0300

    equals added

--- src/LatLngAdapter.java
+++ src/LatLngAdapter.java
@@ -30,6 +30,14 @@ public class LatLngAdapter{
     }
 
     @Override
+    public boolean equals(Object obj){
+        double lat2 = ((LatLngAdapter)obj).getLatitude();
+        double lng2 = ((LatLngAdapter)obj).getLongitude();
+
+        return latitude == lat2 && longitude == lng2;
+    }
+
+    @Override
     public String toString() {
         return "latitude = " + latitude + " longitude = " + longitude;
     }
