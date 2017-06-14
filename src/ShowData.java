f047997d292b1880327fd9d92cbeb66bb88fb08e
John Mitromaras <mitromarasg96@gmail.com>
2017-06-14 18:25:18 +0300

    initial commit

---
+++ src/ShowData.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class ShowData{
+    public static void main(String[] args){
+        try{
+            FileInputStream fi = new FileInputStream(new File("worker_cache"));
+            ObjectInputStream in = new ObjectInputStream(fi);
+
+            ArrayList<PolylineAdapter> polylines = (ArrayList<PolylineAdapter>)in.readObject();
+            for(PolylineAdapter p: polylines){
+                for(LatLngAdapter point: p.getPoints()){
+                    System.out.println(point);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
