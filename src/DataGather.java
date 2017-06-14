9305e6870d6cdcb59c920247aa51d7489e6ad644
John Mitromaras <mitromarasg96@gmail.com>
2017-06-14 18:24:59 +0300

    finished logic, functionality needs to be checked

--- src/DataGather.java
+++ src/DataGather.java
@@ -1,3 +1,6 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.maps.DirectionsApi;
 import com.google.maps.DirectionsApiRequest;
 import com.google.maps.GeoApiContext;
@@ -5,12 +8,57 @@ import com.google.maps.errors.ApiException;
 import com.google.maps.model.DirectionsResult;
 import com.google.maps.model.EncodedPolyline;
 import com.google.maps.model.LatLng;
-import javafx.scene.shape.Polyline;
+import pointInPolygon.Point;
+import pointInPolygon.Polygon;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 public class DataGather{
+
+    private static final File london_file = new File("london.json");
+    private static final File cache_file = new File("worker_cache");
+    private static final Polygon.Builder londonBounds = new Polygon.Builder();
+
+
+    private static LatLng toLatLng(LatLngAdapter latLngAdapter){
+        return new LatLng(latLngAdapter.getLatitude(), latLngAdapter.getLongitude());
+    }
+
+    private static LatLngAdapter toLatLngAdapter(LatLng latLng){
+        return new LatLngAdapter(latLng.lat, latLng.lng);
+    }
+
+    private static void getLondon(){
+        synchronized (london_file){
+            try{
+                JsonParser parser = new JsonParser();
+                FileReader fr = new FileReader(london_file);
+                Object obj = parser.parse(fr);
+
+                JsonObject obj2 = (JsonObject)obj;
+                JsonArray geometries = (JsonArray)obj2.get("geometries");
+                JsonObject geo2 = (JsonObject)geometries.get(0);
+                JsonArray coordinates = (JsonArray)geo2.get("coordinates"); //has two arrays
+                JsonArray array = (JsonArray) coordinates.get(0);
+                JsonArray array0 = (JsonArray) array.get(0);
+
+                Iterator it_inner = array0.iterator();
+                while(it_inner.hasNext()){
+                    JsonArray tuple = (JsonArray)it_inner.next();
+                    double longitude = tuple.get(0).getAsDouble();
+                    double latitude = tuple.get(1).getAsDouble();
+
+                    londonBounds.addVertex(new Point((float)latitude, (float)longitude));
+                }
+            } catch (FileNotFoundException e) {
+                System.err.println("File not found");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         final String ApiKey = "AIzaSyAa5T-N6-BRrJZSK0xlSrWlTh-C7RjOVdY";
 
@@ -20,25 +68,71 @@ public class DataGather{
                 .setReadTimeout(1, TimeUnit.SECONDS)
                 .setWriteTimeout(1, TimeUnit.SECONDS).setApiKey(ApiKey);
 
-        DirectionsApiRequest request = DirectionsApi.newRequest(context).origin("Toronto").destination("Montreal");
-        DirectionsResult result = new DirectionsResult();
+        getLondon();
 
-        try{
-            result = request.await();
-        }catch(InterruptedException e){
-            e.printStackTrace();
-        }catch (IOException e){
-            e.printStackTrace();
-        }catch (ApiException e){
-            e.printStackTrace();
+        ArrayList<PolylineAdapter> polylines = new ArrayList<>();
+
+        for(int i = 0; i < 1; i++) {
+
+            Point dest;
+            Point origin;
+
+            synchronized(londonBounds) {
+                do {
+                    double lat = Math.random();
+                    double lng = Math.random();
+                    origin = new Point((float) lat, (float) lng);
+                } while (londonBounds.build().contains(origin));
+
+                do {
+                    double lat = Math.random();
+                    double lng = Math.random();
+                    dest = new Point((float) lat, (float) lng);
+                } while (londonBounds.build().contains(dest));
+            }
+
+            PolylineAdapter polyline = new PolylineAdapter();
+
+            //return Double.toString(query.hashCode() * Math.random());
+            LatLng o = new LatLng(origin.x, origin.y);
+            LatLng d = new LatLng(dest.x, dest.y);
+            DirectionsApiRequest request = DirectionsApi.newRequest(context).origin(o).destination(d);
+
+            DirectionsResult result;
+            try {
+                result = request.await();
+
+                if (result != null) {
+                    if (result.routes.length > 0) {
+                        EncodedPolyline encPolyline = result.routes[0].overviewPolyline;
+
+                        for (LatLng point : encPolyline.decodePath()) {
+                            polyline.addPoint(toLatLngAdapter(point));
+                        }
+                    }
+                }
+
+                polylines.add(polyline);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        if(result.routes != null && result.routes.length > 0){
-            EncodedPolyline encPolyline = result.routes[0].overviewPolyline;
-            Polyline polyline = new Polyline();
-            for(LatLng point: encPolyline.decodePath()){
-
-                //polyline.getPoints().;
+        synchronized(cache_file){
+            try {
+                FileOutputStream fo = new FileOutputStream(cache_file);
+                ObjectOutputStream out = new ObjectOutputStream(fo);
+                out.writeObject(polylines);
+                out.flush();
+                out.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
